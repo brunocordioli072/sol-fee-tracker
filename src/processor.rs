ff80c2a6df3dd9d1beb981a873f2ecd19653fc12
rdoVieira01 <rdovieira01@gmail.com>
2026-02-14 01:14:17 -0300

    feat: added nozomi accounts

--- src/processor.rs
+++ src/processor.rs
@@ -10,6 +10,7 @@ pub enum Processor {
     Bloxroute,
     Astralane,
     Blockrazor,
+    Nozomi,
 }
 
 impl Processor {
@@ -22,6 +23,7 @@ impl Processor {
             Processor::Bloxroute,
             Processor::Astralane,
             Processor::Blockrazor,
+            Processor::Nozomi,
         ]
     }
 
@@ -95,6 +97,25 @@ impl Processor {
                 "BnGKHAC386n4Qmv9xtpBVbRaUTKixjBe3oagkPFKtoy6",
                 "Dd7K2Fp7AtoN8xCghKDRmyqr5U169t48Tw5fEd3wT9mq",
                 "AP6qExwrbRgBAVaehg4b5xHENX815sMabtBzUzVB4v8S",
+            ],
+            Processor::Nozomi => &[
+                "TEMPaMeCRFAS9EKF53Jd6KpHxgL47uWLcpFArU1Fanq",
+                "noz3jAjPiHuBPqiSPkkugaJDkJscPuRhYnSpbi8UvC4",
+                "noz3str9KXfpKknefHji8L1mPgimezaiUyCHYMDv1GE",
+                "noz6uoYCDijhu1V7cutCpwxNiSovEwLdRHPwmgCGDNo",
+                "noz9EPNcT7WH6Sou3sr3GGjHQYVkN3DNirpbvDkv9YJ",
+                "nozc5yT15LazbLTFVZzoNZCwjh3yUtW86LoUyqsBu4L",
+                "nozFrhfnNGoyqwVuwPAW4aaGqempx4PU6g6D9CJMv7Z",
+                "nozievPk7HyK1Rqy1MPJwVQ7qQg2QoJGyP71oeDwbsu",
+                "noznbgwYnBLDHu8wcQVCEw6kDrXkPdKkydGJGNXGvL7",
+                "nozNVWs5N8mgzuD3qigrCG2UoKxZttxzZ85pvAQVrbP",
+                "nozpEGbwx4BcGp6pvEdAh1JoC2CQGZdU6HbNP1v2p6P",
+                "nozrhjhkCr3zXT3BiT4WCodYCUFeQvcdUkM7MqhKqge",
+                "nozrwQtWhEdrA6W8dkbt9gnUaMs52PdAv5byipnadq3",
+                "nozUacTVWub3cL4mJmGCYjKZTnE9RbdY5AP46iQgbPJ",
+                "nozWCyTPppJjRuw2fpzDhhWbW355fzosWSzrrMYB1Qk",
+                "nozWNju6dY353eMkMqURqwQEoM3SFgEKC6psLCSfUne",
+                "nozxNBgWohjR75vdspfxR5H9ceC7XXH99xpxhVGt3Bb",
             ],
         }
     }
